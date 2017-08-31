c7320a8e6f4f5e1035a174282911399cd7fd4434
johnraychina <johnraychina@163.com>
2017-08-31 16:17:02 +0800

    反射和动态特性对应的map太占用内存，多数场景下用不到，如果需要，可以用BeanUtils代替. 而且没有做递归，当存在多层父子关系时，就无法正确运行（虽然这种情况不多见）；

--- src/main/resources/BasicVo.java
+++ src/main/resources/BasicVo.java
@@ -25,155 +25,83 @@ import java.util.*;
  */
 public abstract class BasicVo implements Serializable {
 
-    private static Logger log = LoggerFactory.getLogger(BasicVo.class);
-
-    //存放当前实体类的属性及类型
-    private HashMap<String, Class<?>> fieldNameTypeMap = new HashMap<String, Class<?>>(10);
-
 
     public BasicVo() {
-
-        //将属性及其类型存入map(fieldName,fieldType)
-        Field[] fields = this.getClass().getDeclaredFields();
-
-        for (int i = 0; i < fields.length; i++) {
-            if ("serialVersionUID".equals(fields[i].getName())) {
-                continue;
-            }
-            fieldNameTypeMap.put(fields[i].getName(), fields[i].getType());
-        }
     }
 
-
-    /**
-     * 根据属性名取值
-     *
-     * @param propertyName 属性名
-     * @return 属性值
-     * @throws Exception
-     */
-    public Object get(String propertyName) throws Exception {
-
-        String methodName = "get";
-        if (propertyName.length() > 1 && Character.isUpperCase(propertyName.charAt(1))) {
-            methodName += propertyName;
-        } else {
-            methodName += (Character.toUpperCase(propertyName.charAt(0)) + propertyName.substring(1));
-        }
-
-        try {
-            //builds the method name, such as: "getXX"
-            Method method = this.getClass().getMethod(methodName,
-                    new Class[]{});
-
-            //Getting value of the field by the method.
-            Object fieldValue = method.invoke(this, new Object[]{});
-
-            return fieldValue;
-
-        } catch (Exception e) {
-            log.error("", e);
-            throw e;
-        }
-    }
-
-
-    /**
-     * 对指定属性设置值
-     *
-     * @param propertyName
-     * @param value
-     * @throws Exception
-     */
-    public void set(String propertyName, Object value) throws Exception {
-
-        String methodName = "set";
-        if (propertyName.length() > 1 && Character.isUpperCase(propertyName.charAt(1))) {
-            methodName += propertyName;
-        } else {
-            methodName += (Character.toUpperCase(propertyName.charAt(0)) + propertyName.substring(1));
-        }
-
-        try {
-            Method method = this.getClass().getMethod(methodName,
-                    new Class[]{(Class<?>) fieldNameTypeMap.get(propertyName)});
-
-            //Setting value of the field by the method.
-            method.invoke(this, new Object[]{value});
-
-        } catch (NoSuchMethodException e) {
-            log.error("", e);
-            throw e;
-        }
-    }
-
-
-    public HashMap<String, Class<?>> fieldNameTypeMap() {
-        return this.fieldNameTypeMap;
-    }
-
-
-    @Override
-    public int hashCode() {
-        int hashCode = 1;
-        Map<String, Object> keyObjectMap = new HashMap<String, Object>();
-        try {
-            for (Iterator<String> it = fieldNameTypeMap.keySet().iterator(); it.hasNext(); ) {
-                String filedName = it.next();
-                Object obj = this.get(filedName);
-                hashCode = 31 * hashCode + (obj == null ? 0 : obj.hashCode());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        keyObjectMap.clear();
-        return hashCode;
-    }
 
     @Override
     public String toString() {
-        Map<String, Object> keyObjectMap = new HashMap<String, Object>();
-        try {
-            for (Iterator<String> it = fieldNameTypeMap.keySet().iterator(); it.hasNext(); ) {
-                String filedName = it.next();
-                keyObjectMap.put(filedName, this.get(filedName));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        String str = JSONObject.toJSONString(keyObjectMap);
-        keyObjectMap.clear();
-        return str;
+        return JSONObject.toJSONString(this);
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        }
-        if (!(o instanceof BasicVo)) {
-            return false;
-        }
+    public int hashCode()
+    {
+        int hashCode = 1;
 
-        BasicVo vo2 = ((BasicVo) o);
-        HashMap<String, Class<?>> ftMap2 = vo2.fieldNameTypeMap();
-        if (fieldNameTypeMap.size() != ftMap2.size()) {
-            return false;
-        }
-
-        try {
-            for (Iterator<String> it = fieldNameTypeMap.keySet().iterator(); it.hasNext(); ) {
-                String filedName = it.next();
-                Object obj1 = this.get(filedName);
-                Object obj2 = vo2.get(filedName);
-                if (!(obj1 == null ? obj2 == null : obj1.equals(obj2))) {
-                    return false;
-                }
+        Field[] fields = this.getClass().getDeclaredFields();
+        try
+        {
+            for( Field field : fields )
+            {
+                Object obj = field.get(this);
+                hashCode = 31 * hashCode + (obj == null ? 0 : obj.hashCode());
             }
-        } catch (Exception e) {
+        }
+        catch (IllegalAccessException e)
+        {
             e.printStackTrace();
         }
 
+        return hashCode;
+    }
+
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == this)
+        {
+            return true;
+        }
+
+        if (this.getClass() != o.getClass() )
+        {
+            return false;
+        }
+
+        Field[] thisFields = this.getClass().getDeclaredFields();
+        Field[] oFields = o.getClass().getDeclaredFields();
+
+        try
+        {
+            for( int i=0; i<thisFields.length; i++ )
+            {
+                if ("serialVersionUID".equals(thisFields[i].getName())) {
+                    continue;
+                }
+
+
+                thisFields[i].setAccessible(true);
+                oFields[i].setAccessible(true);
+                Object thisValue = thisFields[i].get(this);
+                Object oValue = oFields[i].get(o);
+                if( !(thisValue == null ? oValue == null : thisValue.equals(oValue)) )
+                {
+                    return false;
+                }
+            }
+
+        }
+        catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+
+
         return true;
     }
+
 }
+
